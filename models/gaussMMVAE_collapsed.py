fixed marginal likelihood calculation

models/gaussMMVAE_collapsed.py
@@ -180,23 +180,19 @@ class GaussMMVAE(object):
         # compose into stick segments using pi = v \prod (1-v)                                                     
         self.pi_samples = self.compose_stick_segments(v_samples)
 
-        '''
         # sample a component index
         uni_samples = tf.random_uniform((tf.shape(a_inv)[0], self.K), minval=1e-8, maxval=1-1e-8)
         gumbel_samples = -tf.log(-tf.log(uni_samples))
-        component_samples = tf.argmax(tf.log(self.pi_samples) + gumbel_samples, 1)
+        component_samples = tf.to_int32(tf.argmax(tf.log(tf.concat(1, self.pi_samples)) + gumbel_samples, 1))
 
         # calc likelihood term for chosen components
         all_ll = []
-        ll = tf.zeros((batchSize,1))
         for k in xrange(self.K): all_ll.append(-compute_nll(self.X, self.x_recons_linear[k]))
-        for batch_idx in xrange(batchSize): 
-            ll[batch_idx,0] = all_ll[component_samples[batch_idx]][batch_idx,0]
-        '''
+        all_ll = tf.concat(1, all_ll)
 
-        ll = tf.mul(self.pi_samples[0], tf.exp(-compute_nll(self.X, self.x_recons_linear[0])))
-        for k in xrange(self.K-1): ll += tf.mul(self.pi_samples[k+1], tf.exp(-compute_nll(self.X, self.x_recons_linear[k+1])))
-        ll = tf.log(ll)
+        component_samples = tf.concat(1, [tf.expand_dims(tf.range(0,batchSize),1), tf.expand_dims(component_samples,1)])
+        ll = tf.gather_nd(all_ll, component_samples)
+        ll = tf.expand_dims(ll,1)
 
         # calc prior and post terms 
         log_prior = log_normal_pdf(self.z[0], self.mu[0], self.sigma[0])

train_MixModel_VAE-vCollapsed.py
@@ -19,8 +19,8 @@ except ImportError:
 # command line arguments
 flags = tf.flags
 flags.DEFINE_integer("batchSize", 100, "batch size.")
-flags.DEFINE_integer("nEpochs", 50, "number of epochs to train.")
-flags.DEFINE_float("adamLr", 1e-3, "AdaM learning rate.")
+flags.DEFINE_integer("nEpochs", 100, "number of epochs to train.")
+flags.DEFINE_float("adamLr", 3e-4, "AdaM learning rate.")
 flags.DEFINE_integer("hidden_size", 500, "number of hidden units in en/decoder.")
 flags.DEFINE_integer("latent_size", 5, "dimensionality of latent variables.")
 flags.DEFINE_integer("K", 5, "number of components in mixture model.")
@@ -113,7 +113,7 @@ def trainVAE(data, vae_hyperParams, hyperParams, param_save_path, logFile=None):
 
 
 ### Marginal Likelihood Calculation            
-def calc_margLikelihood(data, model, param_file_path, vae_hyperParams, nSamples=100):
+def calc_margLikelihood(data, model, param_file_path, vae_hyperParams, nSamples=50):
     N,d = data.shape
 
     # get op to load the model                                                                                               
@@ -124,11 +124,17 @@ def calc_margLikelihood(data, model, param_file_path, vae_hyperParams, nSamples=
 
         sample_collector = []
         for s_idx in xrange(nSamples):
-            sample_collector.append(s.run(model.get_log_margLL(N), {model.X: data}))
+            samples = s.run(model.get_log_margLL(N), {model.X: data})
+            if not np.isnan(samples.mean()) and not np.isinf(samples.mean()):
+                sample_collector.append(samples)
         
+    if len(sample_collector) < 1:
+        print "\tMARG LIKELIHOOD CALC: No valid samples were collected!"
+        return np.nan
+
     all_samples = np.hstack(sample_collector)
     m = np.amax(all_samples, axis=1)
-    mLL = np.sum(m + np.log(np.mean(np.exp( all_samples - m[np.newaxis].T ), axis=1)))
+    mLL = m + np.log(np.mean(np.exp( all_samples - m[np.newaxis].T ), axis=1))
     return mLL.mean()
 
 